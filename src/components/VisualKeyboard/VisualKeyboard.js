d620a42d210907819f5cd1722e530b4a5ea6ebd3
Amira Basyouni <basyouniamira1@gmail.com>
2025-03-03 16:31:57 -0800

    Completed Stretch Goal: Visual Keyboard see comment section for more detail

--- src/components/VisualKeyboard/VisualKeyboard.js
+++ src/components/VisualKeyboard/VisualKeyboard.js
@@ -3,18 +3,22 @@ import React from "react";
 import { LETTERS } from "../../data.js";
 
 function VisualKeyboard({ keyboardStatus }) {
-	return LETTERS.map((row) => (
-		<p className="guess" key={Math.random()}>
-			{Object.keys(row).map((letter) => (
-				<span
-					className={`cell ${keyboardStatus[letter]}`}
-					key={Math.random()}
-				>
-					{letter}
-				</span>
+	return (
+		<div className="keyboard">
+			{LETTERS.map((row) => (
+				<p className="keyboard-row" key={Math.random()}>
+					{Object.keys(row).map((letter) => (
+						<span
+							className={`key ${keyboardStatus[letter]}`}
+							key={Math.random()}
+						>
+							{letter}
+						</span>
+					))}
+				</p>
 			))}
-		</p>
-	));
+		</div>
+	);
 }
 
 export default VisualKeyboard;
