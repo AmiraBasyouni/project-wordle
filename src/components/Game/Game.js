f2da25ba0460929b7034d934f538902d21ef0961
Amira Basyouni <basyouniamira1@gmail.com>
2025-05-07 06:54:56 -0700

    Watched Solution Video Exercise 4.
    Tips:
    - keep your code simple by checking that a value is empty using variableName ? instead of variableName === '' ?
    - Recall that false literals in JS include '' and undefined

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,13 +2,6 @@ import React from "react";
 import GuessInput from "../GuessInput";
 import GuessLog from "../GuessLog";
 
-import { sample } from "../../utils";
-import { WORDS } from "../../data";
-
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guessLog, setGuessLog] = React.useState([]);
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 import { NUM_OF_LETTERS } from "../../constants";
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+
+/* Pick a random word on every pageload. */
+const answer = sample(WORDS);
+/* To make debugging easier, we'll log the solution in the console. */
+console.info({ answer });
+
 
 function Guess({ guess }) {
-  const IS_GUESS_EMPTY = guess === "";
+  const letter_status = checkGuess(guess, answer);
+
+  function fetchStatus(i) {
+    return guess ? ` ${letter_status[i]["status"]}` : '';
+  }
 
   /* Render each guess letter. If guess is empty, render a row of empty cells. */
   return (
     <p className="guess">
       {range(NUM_OF_LETTERS).map((i) => (
-        <span key={i} className="cell">
-          {IS_GUESS_EMPTY ? undefined : guess[i]}
+        <span key={i} className={"cell" + fetchStatus(i)}>
+          {guess ? guess[i] : undefined}
         </span>
       ))}
     </p>
